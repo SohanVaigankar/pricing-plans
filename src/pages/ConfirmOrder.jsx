d9b2c2ec2dbeceff4dc9e4d445f1c9517d2a74cb
sohan vaigankar <svaigankar2000@gmail.com>
2022-12-16 21:06:23 +0530

    fix: border

--- src/pages/ConfirmOrder.jsx
+++ src/pages/ConfirmOrder.jsx
@@ -25,9 +25,9 @@ const ConfirmOrder = () => {
   return (
     <>
       <Navbar />
-      <div className="p-5 w-[95%] md:w-[90%] lg:w-[80%] mx-auto flex justify-center  items-center py-10 mt-20 ">
-        <div className="flex flex-col items-center  mx-auto w-[100%] sm:w-[75%] lg:w-[60%] 2xl:w-[40%]  rounded shadow-lg">
-          <p className="w-full text-center px-2 py-2 text-[1.5rem] border-b-[1px] text-[#4545c0]">
+      <div className="p-5 w-[95%] md:w-[90%] lg:w-[80%] mx-auto flex justify-center  items-center py-10 mt-20  ">
+        <div className="flex flex-col items-center  mx-auto w-[100%] sm:w-[75%] lg:w-[60%] 2xl:w-[40%]  rounded border-[1px]  border-[#4545c0ab]">
+          <p className="w-full text-center px-2 py-2 text-[1.5rem] border-b-[1px] border-[#4545c0ab] text-[#4545c0]">
             Confirm Order?
           </p>
           <div className="flex flex-col   px-10 py-10 text-[1.1rem] w-full gap-5 font-[500]">
